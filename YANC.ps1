43f4686936bccbd821fdd4dc7d6bf350f87f3053
Shaiju Rajan <s.rajan@devon.nl>
2019-08-22 15:55:32 +0530

    Latest yanc.ps1

--- YANC.ps1
+++ YANC.ps1
@@ -9,50 +9,79 @@ foreach($package in $packages)
      "`n"
 
      $packageName=$package.Name
+
      Write-Output "Processing package - $packageName.."
+
      $versions=$package.Group|select -ExpandProperty Versions -Unique|sort -desc
      $maxVersion=$versions|select -First 1
-     $versionstring=$versions -join ','
-     $applicableProjects=$package.Group.ProjectName -join ','
      
      Write-Output "Finding the latest stable version of $packageName.."
 
-     $candidateVersion=Find-Package -Id $package.Name -ExactMatch
-     if($candidateVersion -ne $null)
-     {
-        $candidateVersion=$candidateVersion.Version.ToString()
-     }
+     $latestPackage=Find-Package -Id $package.Name -ExactMatch
 
+     $candidateVersion=if($latestPackage -ne $null){$latestPackage.Version}else{$maxVersion}
 
      #If consolidated version is already applied to all the projects, skip the package
-     if(($versions.Count -eq 1) -and ($versions[0] -eq  $maxVersion))
+     if(($versions.Count -eq 1) -and ($versions[0].Version -eq  $candidateVersion.Version))
      {
         "$packageName is already up-to-date in all the projects.."
         continue
      }
-
-     $details=[pscustomobject]@{Name=$package.Name; Versions=$versionstring;MaxInstalledVersion=$maxVersion;MaxStableVersion=$candidateVersion;ApplicableProjects=$applicableProjects;}
+     
+     $details=[pscustomobject]@{
+        Name=$package.Name; 
+        Versions=$($versions -join ',');
+        MaxInstalledVersion=$maxVersion;
+        CandidateVersion=$candidateVersion;
+        applicableProjects=$($package.Group.ProjectName -join ',');
+        }
      
      Write-Output "Details -$details"
      
      $result+=$details
-
+    
+     #$count++
+     #
+     #if($count -eq 1)
+     #{
+     #   break
+     #}
 }
 
-#select -Property  Name,Versions,MaxInstalledVersion,MaxStableVersion
-$result |Export-Csv "C:\Temp\depsnew.csv"
 
-$consolidatedPrjNames=$result|select -ExpandProperty ApplicableProjects -Unique
+$result|Export-Csv "C:\Temp\depsnew.csv"
 
-foreach($prjName in $consolidatedPrjNames)
+$consolidatedPrjNames=$result|select -ExpandProperty applicableProjects -Unique
+
+foreach($prjName in $consolidatedPrjNames.Split(','))
 {
-        "Checking the applicable updates of $prjName"
-        "-----------------------------------------"
-        $consolidatedPackages=$result|? {$_.ApplicableProjects.Contains($prjName)}
-        foreach($pkg in $consolidatedPackages)
+        
+
+        #"Checking the applicable updates of $prjName"
+        #"-----------------------------------------"
+        #$consolidatedPackages=$result|? {$_.applicableProjects.Split(',').Contains($prjName)}
+        #foreach($pkg in $consolidatedPackages)
+        #{
+        #    "Consolidating Package $($pkg.Name) in $prjName"
+        #    
+        #    #$installedPackage=get-package $pkg.Name -ProjectName $prjName
+        #    #
+        #    #"Currently installed : $($installedPackage.Version.Version.ToString())"
+        #    #"Candidate : $( $pkg.CandidateVersion)"
+        #    
+        #    Update-Package $pkg.Name -ProjectName $prjName 
+        #}
+
+
+        Try
         {
-            "Consolidating Package$($pkg.Name) in $prjName"
-            #Update
+            Update-Package -ProjectName $prjName 
+            "Updated all packages in $prjName" | Out-File "C:\Temp\updatelog.txt"
+        }
+        Catch
+        {
+            $ErrorMessage = $_.Exception.Message
+            "Error updating packages in $prjName : $ErrorMessage" | Out-File "C:\Temp\updatelog.txt"
         }
 }
 
